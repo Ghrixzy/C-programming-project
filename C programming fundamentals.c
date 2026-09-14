dcf29241181b36a351301fe5b5e6f57a3299e46a
Godson Appiah <adhivybz874@gmail.com>
2026-09-14 05:16:31 -0700

    Add files via upload

---
+++ C programming fundamentals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("C PROGRAMMING FUNDAMENTALS\n");
+    printf("Name: Evans Opoku\n");
+    printf("Student ID: 01255148B\n");
+    printf("Programme: Biomedical Engineering\n");
+    printf("Welcome to C Programming\n");
+
+    return 0;
+}
